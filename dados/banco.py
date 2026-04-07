16034a1a0cc4ceaacc9b1e428e37bcb78580bbef
Daniel da silva <foconoaltar123@gmail.com>
2026-04-06 23:53:03 -0300

    fix: erro na senha do banco

--- dados/banco.py
+++ dados/banco.py
@@ -2,17 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 import json
 from src.modelos.models import Elenco, Base
-
+from urllib.parse import quote
 from src.Erros_personalizado.erros import *
 import os
 from dotenv import load_dotenv
 
+user = os.getenv("DB_USER")
+password = quote(os.getenv("PASSWORD"), safe="")
+host = os.getenv("DB_HOST")
+db = os.getenv("DB_NAME")
+schema = os.getenv("SCHEMA")
 
-load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"password": os.getenv("PASSWORD"),
-        "options": f"-csearch_path={os.getenv('SCHEMA')}"})
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{db}"
 
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"options": f"-csearch_path={schema}"}
+)
 
 Base.metadata.create_all(engine)
 
